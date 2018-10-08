6902e10d0f28ea1af3774ff24c3f82bdf19784f4
Joao Rabello <rabellois@hotmail.com>
2018-10-08 15:37:22 -0300

    Colisões detectáveis
    -Adicionada variável que controla qual é o card e de que mulher é a partir de um valor int público.
    -Por enquanto debug,logs fazem o papel de "surgir o canvas"

---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour {
+
+    public int actualWoman;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Item"))
+        {
+            Debug.Log("Entrou no item");
+            if(actualWoman == 1)
+            {
+                //Aparece Canvas 1
+                Debug.Log("Canvas 1");
+            }
+            else
+            {
+                if(actualWoman == 2)
+                {
+                    //Aparece Canvas 2
+                    Debug.Log("Canvas 2");
+                }
+                else
+                {
+                    if(actualWoman == 3)
+                    {
+                        //Aparece Canvas 3
+                        Debug.Log("Canvas 3");
+                    }
+                }
+            }
+        }
+    }
+}
